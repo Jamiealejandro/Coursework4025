67bf81cd5f39c8b28bf0f8cc617050f6a72194f7
Jamie Chapman <34374063+Jamiealejandro@users.noreply.github.com>
2021-03-20 13:37:32 +0000

    fixedmongo

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var mdbpass = PASS
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://jamiealejandro:<Damansara1>@cluster0.rhco3.mongodb.net/cluster0?retryWrites=true&w=majority";
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, {
+useNewUrlParser: true, useUnifiedTopology: true});
 client.connect(err => {
 const collection = client.db("test").collection("devices");
 //  // perform actions on the collection object
